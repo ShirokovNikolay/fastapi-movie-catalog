dd0d3655946b30c7c2ee7fe328933bc479bde3d1
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-14 22:10:19 +0300

    Remade view create_movie using Form() for parameters name, description, rating. Parameter movie_id equals to random number from 1 to 10.

--- movie-catalog/api/api_v1/movies/views.py
+++ movie-catalog/api/api_v1/movies/views.py
@@ -1,35 +1,23 @@
 import random
 from typing import Annotated
 
+from annotated_types import MaxLen
 from fastapi import (
     Depends,
     APIRouter,
     status,
     Form,
 )
+from pydantic import Field
 
 from .crud import MOVIES
 from .dependencies import prefetch_movie
-from schemas.movie import Movie, MovieBase
+from schemas.movie import Movie
 
 router = APIRouter(
     prefix="/movies",
     tags=["Movies"],
 )
-
-
-@router.post(
-    "/",
-    response_model=Movie,
-    status_code=status.HTTP_201_CREATED,
-)
-def create_movie(
-    movie_in: Annotated[MovieBase, Form()],
-):
-    return Movie(
-        movie_id=random.randint(1, 10),
-        **movie_in.model_dump(),
-    )
 
 
 @router.get(
@@ -38,6 +26,36 @@ def create_movie(
 )
 def read_movies_list():
     return MOVIES
+
+
+@router.post(
+    "/",
+    response_model=Movie,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_movie(
+    name: Annotated[
+        str,
+        MaxLen(20),
+        Form(),
+    ],
+    description: Annotated[
+        str,
+        MaxLen(200),
+        Form(),
+    ],
+    rating: Annotated[
+        int,
+        Field(ge=1, le=10),
+        Form(),
+    ],
+):
+    return Movie(
+        movie_id=random.randint(1, 10),
+        name=name,
+        description=description,
+        rating=rating,
+    )
 
 
 @router.get("/{movie_id}/")
