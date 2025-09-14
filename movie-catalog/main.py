09d58fa94c0c0837b2147fc97db14ab1c59cbd2d
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-14 17:36:40 +0300

    Create views read_movies_list, read_movie_details. Add dependence prefetch_movie. Add list MOVIES.

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,11 +1,38 @@
+from typing import Annotated
+
 from fastapi import (
     FastAPI,
     Request,
+    HTTPException,
+    status,
+    Depends,
 )
+
+from schemas.movie import Movie
 
 app = FastAPI(
     title="Movie Catalog",
 )
+MOVIES = [
+    Movie(
+        movie_id=1,
+        name="name 1",
+        description="description 1",
+        rating=5,
+    ),
+    Movie(
+        movie_id=2,
+        name="name 2",
+        description="description 2",
+        rating=7,
+    ),
+    Movie(
+        movie_id=3,
+        name="name 3",
+        description="description 3",
+        rating=8,
+    ),
+]
 
 
 @app.get("/")
@@ -21,3 +48,34 @@ def read_root(
         "message": f"Hello {name}",
         "docs": str(docs_url),
     }
+
+
+@app.get(
+    "/movies/",
+    response_model=list[Movie],
+)
+def read_movies_list():
+    return MOVIES
+
+
+def prefetch_movie(movie_id: int) -> Movie:
+    movie: Movie | None = next(
+        (movie for movie in MOVIES if movie.movie_id == movie_id),
+        None,
+    )
+    if movie:
+        return movie
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Movie with {movie_id=} not found",
+    )
+
+
+@app.get("/movies/{movie_id}/")
+def read_movie_details(
+    movie: Annotated[
+        Movie,
+        Depends(prefetch_movie),
+    ],
+):
+    return movie
