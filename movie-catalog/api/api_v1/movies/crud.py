84637d345ed3a9ba62f5a3319dc722530c8feca4
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-15 17:53:23 +0300

    Add MovieStorage with methods get, get_by_slug, create

--- movie-catalog/api/api_v1/movies/crud.py
+++ movie-catalog/api/api_v1/movies/crud.py
@@ -1,22 +1,36 @@
-from schemas.movie import Movie
+from schemas.movie import Movie, MovieCreate
 
-MOVIES = [
-    Movie(
+
+class MovieStorage:
+    slug_to_movie: dict[str, Movie] = {}
+
+    def get(self) -> list[Movie]:
+        return list(self.slug_to_movie.values())
+
+    def get_by_slug(self, slug) -> Movie | None:
+        return self.slug_to_movie.get(slug)
+
+    def create(self, create_movie: MovieCreate) -> Movie:
+        movie = Movie(**create_movie.model_dump())
+        self.slug_to_movie[movie.slug] = movie
+        return movie
+
+
+storage = MovieStorage()
+storage.create(
+    MovieCreate(
         slug="search",
         name="name 1",
         description="description 1",
         rating=5,
     ),
-    Movie(
+)
+
+storage.create(
+    MovieCreate(
         slug="leetcode",
         name="name 2",
         description="description 2",
         rating=7,
     ),
-    Movie(
-        slug="example",
-        name="name 3",
-        description="description 3",
-        rating=8,
-    ),
-]
+)
