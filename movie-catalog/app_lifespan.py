5298f0f4c4de79dcbbc4266af15f574f9816e751
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-20 12:15:39 +0300

    Add lifespan.

---
+++ movie-catalog/app_lifespan.py
@@ -0,0 +1,11 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from api.api_v1.movies.crud import storage
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> None:
+    storage.init_storage_from_state()
+    yield
