cbf76dd300d87ce69a712c9faa1f357cfde1caef
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-21 13:14:05 +0300

    Add BackgroundTasks for save_state in views.

--- movie-catalog/api/api_v1/movies/views/list_views.py
+++ movie-catalog/api/api_v1/movies/views/list_views.py
@@ -2,6 +2,8 @@ from fastapi import (
     APIRouter,
     status,
 )
+from fastapi import BackgroundTasks
+
 from api.api_v1.movies.crud import storage
 from schemas.movie import (
     Movie,
@@ -30,5 +32,7 @@ def read_movies_list() -> list[Movie]:
 )
 def create_movie(
     movie_create: MovieCreate,
+    background_tasks: BackgroundTasks,
 ) -> Movie:
+    background_tasks.add_task(storage.save_state)
     return storage.create(movie_create)
