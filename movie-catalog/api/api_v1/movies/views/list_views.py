9f6a39e6b26f33d96c5157ac2228f074433f05cc
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-21 15:05:50 +0300

    Add dependence save_storage_state in router. Remove dependence from views.

--- movie-catalog/api/api_v1/movies/views/list_views.py
+++ movie-catalog/api/api_v1/movies/views/list_views.py
@@ -1,10 +1,11 @@
 from fastapi import (
     APIRouter,
     status,
+    Depends,
 )
-from fastapi import BackgroundTasks
 
 from api.api_v1.movies.crud import storage
+from api.api_v1.movies.dependencies import save_storage_state
 from schemas.movie import (
     Movie,
     MovieCreate,
@@ -14,6 +15,7 @@ from schemas.movie import (
 router = APIRouter(
     prefix="/movies",
     tags=["Movies"],
+    dependencies=[Depends(save_storage_state)],
 )
 
 
@@ -30,9 +32,5 @@ def read_movies_list() -> list[Movie]:
     response_model=MovieRead,
     status_code=status.HTTP_201_CREATED,
 )
-def create_movie(
-    movie_create: MovieCreate,
-    background_tasks: BackgroundTasks,
-) -> Movie:
-    background_tasks.add_task(storage.save_state)
+def create_movie(movie_create: MovieCreate) -> Movie:
     return storage.create(movie_create)
