58531aca4cd996cdf1cc8270a207bfeafedbff12
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-24 16:43:29 +0300

    Add dependence api_token_or_basic_auth_required_for_unsafe_methods in router.

--- movie-catalog/api/api_v1/movies/views/list_views.py
+++ movie-catalog/api/api_v1/movies/views/list_views.py
@@ -7,8 +7,7 @@ from fastapi import (
 from api.api_v1.movies.crud import storage
 from api.api_v1.movies.dependencies import (
     save_storage_state,
-    api_token_required_for_unsafe_methods,
-    user_basic_auth_required_for_unsafe_methods,
+    api_token_or_basic_auth_required_for_unsafe_methods,
 )
 from schemas.movie import (
     Movie,
@@ -21,8 +20,7 @@ router = APIRouter(
     tags=["Movies"],
     dependencies=[
         Depends(save_storage_state),
-        # Depends(api_token_required_for_unsafe_methods),
-        Depends(user_basic_auth_required_for_unsafe_methods),
+        Depends(api_token_or_basic_auth_required_for_unsafe_methods),
     ],
     responses={
         status.HTTP_401_UNAUTHORIZED: {
