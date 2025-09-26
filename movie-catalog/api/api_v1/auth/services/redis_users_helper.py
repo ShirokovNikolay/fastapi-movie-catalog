2d9daed996aed76bc441990e9b643185a1ace7e1
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-26 03:05:47 +0300

    redis.py -> tokens_helper.py + redis_tokens_helper.py and users_helper.py + redis_users_helper.py.

---
+++ movie-catalog/api/api_v1/auth/services/redis_users_helper.py
@@ -0,0 +1,29 @@
+from redis import Redis
+
+from core import config
+from .users_helper import AbstractUsersHelper
+
+
+class RedisUsersHelper(AbstractUsersHelper):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        db: int,
+    ) -> None:
+        self.redis = Redis(
+            host=host,
+            port=port,
+            db=db,
+            decode_responses=True,
+        )
+
+    def get_user_password(self, username: str) -> str | None:
+        return self.redis.get(username)
+
+
+redis_users = RedisUsersHelper(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB_USERS,
+)
