14b39ba4a89956a51206b6e14a7b04b328b71c49
Nikolay <shirkol2006@mail.ru>
2025-10-18 19:08:52 +0300

    Test case: create movie with already exists slug.

--- movie-catalog/testing/test_api/test_api_v1/test_movies/test_views/test_list_views/test_create.py
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_views/test_list_views/test_create.py
@@ -5,7 +5,7 @@ from fastapi import status
 from starlette.testclient import TestClient
 
 from main import app
-from schemas.movie import MovieCreate
+from schemas.movie import Movie, MovieCreate
 
 
 def test_create_movie(auth_client: TestClient) -> None:
@@ -27,3 +27,14 @@ def test_create_movie(auth_client: TestClient) -> None:
     response_data = response.json()
     received_values = MovieCreate(**response_data)
     assert received_values == movie_create, response_data
+
+
+def test_create_movie_already_exists(auth_client: TestClient, movie: Movie) -> None:
+    movie_create = MovieCreate(**movie.model_dump())
+    data: dict[str, str] = movie_create.model_dump(mode="json")
+    url = app.url_path_for("create_movie")
+    response = auth_client.post(url=url, json=data)
+    assert response.status_code == status.HTTP_409_CONFLICT, response.text
+    response_data = response.json()
+    expected_error_detail = f"Movie with slug={movie_create.slug!r} already exists."
+    assert response_data["detail"] == expected_error_detail, response_data
