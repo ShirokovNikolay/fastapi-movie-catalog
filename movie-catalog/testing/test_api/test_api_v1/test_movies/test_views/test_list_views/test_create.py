2494ad52075e7b7ddc574cf3087576cd00c90658
Nikolay <shirkol2006@mail.ru>
2025-10-19 18:02:54 +0300

    Create class TestCreateInvalidMovie: Use parametrize fixture movie_create_values in test invalid_movie_slug.

--- movie-catalog/testing/test_api/test_api_v1/test_movies/test_views/test_list_views/test_create.py
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_views/test_list_views/test_create.py
@@ -1,11 +1,15 @@
 import random
 import string
+from typing import Any
 
+import pytest
+from _pytest.fixtures import SubRequest
 from fastapi import status
 from starlette.testclient import TestClient
 
 from main import app
 from schemas.movie import Movie, MovieCreate
+from testing.conftest import build_movie_create_random_slug
 
 
 def test_create_movie(auth_client: TestClient) -> None:
@@ -38,3 +42,41 @@ def test_create_movie_already_exists(auth_client: TestClient, movie: Movie) -> N
     response_data = response.json()
     expected_error_detail = f"Movie with slug={movie_create.slug!r} already exists."
     assert response_data["detail"] == expected_error_detail, response_data
+
+
+class TestCreateInvalidMovie:
+    @pytest.fixture(
+        params=[
+            pytest.param(
+                ("s", "string_too_short"),
+                id="too-short-slug",
+            ),
+            pytest.param(
+                ("s" * 11, "string_too_long"),
+                id="too-long-slug",
+            ),
+        ],
+    )
+    def movie_create_values(self, request: SubRequest) -> tuple[dict[str, Any], str]:
+        build = build_movie_create_random_slug()
+        data = build.model_dump(mode="json")
+        slug, error_type = request.param
+        data["slug"] = slug
+        return data, error_type
+
+    def test_invalid_movie_slug(
+        self,
+        movie_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_movie")
+        create_data, expected_error_type = movie_create_values
+        response = auth_client.post(
+            url=url,
+            json=create_data,
+        )
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        error_detail = response.json()["detail"][0]
+        assert error_detail["type"] == expected_error_type, error_detail
