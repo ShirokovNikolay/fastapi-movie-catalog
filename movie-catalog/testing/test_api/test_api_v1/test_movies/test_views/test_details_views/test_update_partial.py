e3d148a08363e6364ec52285db80993f9570632f
Nikolay <shirkol2006@mail.ru>
2025-10-19 22:31:29 +0300

    Test update_movie_details_partial view using fixture movie with indirect parametrize.

---
+++ movie-catalog/testing/test_api/test_api_v1/test_movies/test_views/test_details_views/test_update_partial.py
@@ -0,0 +1,68 @@
+from collections.abc import Generator
+
+import pytest
+from _pytest.fixtures import SubRequest
+from starlette import status
+from starlette.testclient import TestClient
+
+from api.api_v1.movies.crud import storage
+from main import app
+from schemas.movie import DESCRIPTION_MAX_LENGTH, Movie
+from testing.conftest import create_movie
+
+
+class TestUpdatePartial:
+    @pytest.fixture
+    def movie(self, request: SubRequest) -> Generator[Movie]:
+        slug, description = request.param
+        movie = create_movie(
+            slug=slug,
+            description=description,
+        )
+        yield movie
+        storage.delete(movie)
+
+    @pytest.mark.parametrize(
+        "movie, new_description",
+        [
+            pytest.param(
+                ("slug", "description"),
+                "",
+                id="description-to-empty-description",
+            ),
+            pytest.param(
+                ("empty", ""),
+                "not empty description",
+                id="empty-description-to-description",
+            ),
+            pytest.param(
+                ("max-to-min", "s" * DESCRIPTION_MAX_LENGTH),
+                "",
+                id="max-description-to-empty-description",
+            ),
+            pytest.param(
+                ("min-to-max", ""),
+                "s" * DESCRIPTION_MAX_LENGTH,
+                id="empty-description-to-max-description",
+            ),
+        ],
+        indirect=["movie"],
+    )
+    def test_update_movie_details_partial(
+        self,
+        movie: Movie,
+        new_description: str,
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for(
+            "update_movie_details_partial",
+            slug=movie.slug,
+        )
+        response = auth_client.patch(
+            url=url,
+            json={"description": new_description},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        movie_db = storage.get_by_slug(slug=movie.slug)
+        assert movie_db
+        assert movie_db.description == new_description
